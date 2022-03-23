8560661e3aaacd16db3dafa3c6a9c1d2845db725
andrinr <andrinrehmann@gmail.com>
2022-03-23 11:40:11 +0100

    some error needs fixing

--- life/main.py
+++ life/main.py
@@ -1,20 +1,33 @@
+from glob import glob
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.ndimage import convolve
+from matplotlib.animation import FuncAnimation
 
 x = 50
 y = 50
 
 grid = np.random.rand(x, y) > 0.8
 
-print(grid)
-
 # Define Kernels
 kernel = np.ones((3,3))
 kernel[1,1] = 0
 
-def step():
+fig, ax = plt.subplots()
+imshow = plt.imshow(grid)
+
+def update(i):
+    global grid
+
     surround = convolve(grid, kernel, mode='constant', cval=0)
 
-    new = surround == 3 and not grid
-    new = not(surround <= 1 and grid) and grid
-    dead = 
+    grid = \
+        (surround == 3 and not grid) or \
+        (surround >= 2 and surround <= 3 and grid)
+
+    imshow.set_data(grid)
+
+
+ani = FuncAnimation(fig, update, frames=np.linspace(0, 1000, 1), repeat=True)
+
+plt.show()
