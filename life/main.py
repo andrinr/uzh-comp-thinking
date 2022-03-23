6e5e79f9c41060b842aba341ba8ed3f4239a1509
andrinr <andrinrehmann@gmail.com>
2022-03-23 16:08:40 +0100

    almost done with live, some error

--- life/main.py
+++ life/main.py
@@ -4,30 +4,35 @@ import matplotlib.pyplot as plt
 from scipy.ndimage import convolve
 from matplotlib.animation import FuncAnimation
 
-x = 50
-y = 50
+x = 20
+y = 20
 
-grid = np.random.rand(x, y) > 0.8
+grid = np.random.rand(x, y) > 0.7
 
 # Define Kernels
 kernel = np.ones((3,3))
 kernel[1,1] = 0
 
+print(kernel)
 fig, ax = plt.subplots()
 imshow = plt.imshow(grid)
 
 def update(i):
     global grid
 
-    surround = convolve(grid, kernel, mode='constant', cval=0)
-
+    surround = convolve(grid.astype(int), kernel, mode='constant', cval=0)
+    print(grid)
+    print(surround)
     grid = \
-        (surround == 3 and not grid) or \
-        (surround >= 2 and surround <= 3 and grid)
+        np.logical_or(\
+            np.logical_and(surround == 3, np.logical_not(grid)),\
+            np.logical_and(np.logical_and(surround >= 2, surround <= 3), grid))
 
     imshow.set_data(grid)
 
+    print(i)
 
-ani = FuncAnimation(fig, update, frames=np.linspace(0, 1000, 1), repeat=True)
+
+ani = FuncAnimation(fig, update, frames=np.linspace(0, 1000, 1), interval=1000, repeat=True)
 
 plt.show()
