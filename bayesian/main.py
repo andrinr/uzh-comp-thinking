68872d787482c22c8a8514cfece3f5eab838bd3b
andrinr <andrinrehmann@gmail.com>
2022-05-04 14:01:40 +0200

    multivariate normal assumption also working now

--- bayesian/main.py
+++ bayesian/main.py
@@ -7,7 +7,7 @@ from scipy.stats import norm
 from scipy.stats import multivariate_normal
 from sklearn.model_selection import train_test_split
 
-nLabels, minSamples, maxSamples, testSamples =4, 100, 300, 20
+nLabels, minSamples, maxSamples, testSamples = 3, 200, 400, 20
 
 for i in range(nLabels):
 
@@ -26,7 +26,6 @@ for i in range(nLabels):
 
 X = samples[:,0:2]
 y = samples[:,2]
-
 
 
 def bayes_class_ind(x_train, y_train, x):
@@ -68,12 +67,12 @@ def bayes_class(x_train, y_train, x):
         p_label = counts[i] / rows
 
         mn = multivariate_normal(
-            np.mean(x_train[y_train == label, :]), 
-            np.var(x_train[y_train == label, :])
+            np.mean(x_train[y_train == label, :], axis=0), 
+            np.cov(x_train[y_train == label, :], rowvar=False)
             )
 
         p_xi = mn.pdf(x)
-        print(p_xi)
+
         p = p_label * p_xi
 
         if (p > maxP):
@@ -89,10 +88,6 @@ y_pred_ind = np.zeros(len(X_test))
 for i in range(len(X_test)):
     y_pred_ind[i] = bayes_class_ind(X_train, y_train, X_test[i])
 
-        
-y_pred = np.zeros(len(X_test))
-for i in range(len(X_test)):
-    y_pred[i] = bayes_class(X_train, y_train, X_test[i])
 
 
 fig, ax = plt.subplots()
@@ -100,9 +95,19 @@ ax.scatter(X_train[:,0], X_train[:,1], c=y_train)
 ax.scatter(X_test[:,0], X_test[:,1], c=y_pred_ind,  marker="x")
 ax.set_title("Assuming ind. normal distr.")
 
+print("Percentage of incorrect classifications with ind. distr.: " , np.sum(y_pred_ind != y_test) / len(y_test))
+
+y_pred = np.zeros(len(X_test))
+for i in range(len(X_test)):
+    y_pred[i] = bayes_class(X_train, y_train, X_test[i])
+
 fig, ax = plt.subplots()
 ax.scatter(X_train[:,0], X_train[:,1], c=y_train)
 ax.scatter(X_test[:,0], X_test[:,1], c=y_pred,  marker="x")
 ax.set_title("Assuming multivariate normal distr.")
+
+
+print("Percentage of incorrect classifications with multivariate distr.: " , np.sum(y_pred != y_test) / len(y_test))
+
 
 plt.show()
