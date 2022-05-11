1c2c070f7da473cd2b04205c1c8c697e4ed11e4b
andrinr <andrinrehmann@gmail.com>
2022-05-11 19:26:41 +0200

    finish model selection

--- bayesian-model-selection/main.py
+++ bayesian-model-selection/main.py
@@ -4,22 +4,29 @@ import random as rnd
 from scipy.stats import binom
 from scipy.stats import beta
 
-
-thetas = [0.5, 0.65, 0.8]
+n = 100
+Hs = [50, 75, 80]
 
 # discrete integration, cause why not
-def integrate(f, l, r, s):
-    sum = 0
+def integrate(f, l, r, s, k):
+    sum = 0.0
     for x in np.arange(l,r, s):
-        sum += f(x)
-
-
-def m_0(x):
-    binom(100, x)
-
-def m_1(x):
-    binom(100, x) * beta.pdf(x, 30, 30)
-
-for theta in thetas:
-    m_0 = binom(100, theta) * 
+        sum += f(k, x) * s
     
+    print(sum)
+    return sum
+
+
+def m_0(k, x):
+    return binom.pmf(k, n, x)
+
+def m_1(k, x):
+    return binom.pmf(k, n, x) * beta.pdf(x, 30, 30)
+
+factors = []
+for H in Hs:
+    factors.append(integrate(m_0, 0, 1, 0.001, H) / integrate(m_1, 0, 1, 0.001, H))
+
+
+print(factors)
+
