5b88b0c9c8f318ea2cac52b67b9b7258dd8db862
andrinr <andrinrehmann@gmail.com>
2022-10-05 23:31:05 +0200

    primitive form of hashlife"

--- life/hash.py
+++ life/hash.py
@@ -2,28 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.ndimage import convolve
 from matplotlib.animation import FuncAnimation
+import cProfile
 
 import time
-
 
 # Define Kernels
 kernel = np.ones((3,3))
 kernel[1,1] = 0
 
-n_grid = 8
-n_supergrid = 5
+n_grid = 512 + 2
+n_supergrid = 16
 
 supergrid = np.zeros((n_supergrid, n_supergrid, n_grid, n_grid))
-print(np.shape(supergrid))
+supergrid[2, 2, 1:-1, 1:-1] = np.random.rand(n_grid - 2, n_grid - 2) > 0.7
 
 map = dict()
 flat = np.zeros((n_supergrid * n_grid, n_supergrid * n_grid))
 
 def grid_to_hash(grid):
-    return grid.tobytes()
-
-def hash_to_grid(hash):
-    return np.frombuffer(hash, dtype=int)
+    a = sum(grid)
+    a *= np.linspace(1, 9, num=n_grid)
+    return sum(a)
 
 def evolve_grid(grid):
     surround = convolve(grid.astype(int), kernel, mode='wrap', cval=0)
@@ -31,57 +30,74 @@ def evolve_grid(grid):
         np.logical_or(surround == 3,
         np.logical_and(surround == 2, grid))
 
+    return grid
+
 def flatten(flat, supergrid, n_supergrid, n_grid):
     
     for i in range(n_supergrid):
         for j in range(n_supergrid):
             flat[i*n_grid:(i+1)*n_grid, j*n_grid:(j+1)*n_grid] = supergrid[i, j, :, :]
 
-    
+
+flatten(flat, supergrid, n_supergrid, n_grid)    
 fig, ax = plt.subplots()
 imshow = plt.imshow(flat)
 
+speedup = 10
 def update(t):
 
-    # center
-    for i in range(n_supergrid):
-        for j in range(n_supergrid):
-            hash = grid_to_hash(supergrid[i, j, :, :])
-            print(hash)
-            if hash in map:
-                print(hash_to_grid(map[hash]))
-                supergrid[i, j, :, :] = hash_to_grid(map[hash])
-            else:
-                tmp_hash = grid_to_hash(supergrid[i, j, :, :])
-                evolve_grid(supergrid[i, j, :, :])
-                map[tmp_hash] = grid_to_hash(supergrid[i, j, :, :])
+    for k in range(speedup):
+        print('update ', t)
+        # center
+        for i in range(n_supergrid):
+            for j in range(n_supergrid):
+                #print('shape', np.shape(supergrid[i, j, :, :] ))
+                hash = grid_to_hash(supergrid[i, j, :, :])
+                
+                if hash in map:
+                    next_hash, supergrid[i, j, :, :] = map[hash]
+                    #print(t, i, j, 'hash found', hash)
+                else:
+                    #print('before ', supergrid[i, j, :, :])
+                    supergrid[i, j, :, :]= evolve_grid(supergrid[i, j, :, :])
+                    #print('after ', supergrid[i, j, :, :])
+                    cp = np.zeros((n_grid, n_grid))
+                    cp = np.copy(supergrid[i, j, :, :])
+                    map[hash] = ( grid_to_hash(cp), cp)
+                    #print(t, i, j, 'hash not found', hash)
 
-    # update ghost cells
-    for i in range(n_supergrid):
-        for j in range(n_supergrid):
-            
-            # Reset ghost cells from self
-            supergrid[i, j, 0:-1, 0] = 0
-            supergrid[i, j, 0:-1, -1] = 0
-            supergrid[i, j, 0, 0:-1] = 0
-            supergrid[i, j, -1, 0:-1] = 0
+        # update ghost cells
+        for i in range(n_supergrid):
+            for j in range(n_supergrid):
+                
+                # Reset ghost cells from self
+                supergrid[i, j, :, 0] = 0
+                supergrid[i, j, :, -1] = 0
+                supergrid[i, j, 0, :] = 0
+                supergrid[i, j, -1, :] = 0
 
-            # corner lines
-            supergrid[i, j, 1:-2, 0] = supergrid[i, (j+1)%n_supergrid, 1:-2, -2]
-            supergrid[i, j, 1:-2, -1] = supergrid[i, (j-1)%n_supergrid, 1:-2, 1]
-            supergrid[i, j, 0, 1:-2] = supergrid[(i+1)%n_supergrid, j, -2, 1:-2]
-            supergrid[i, j, -1, 1:-2] = supergrid[(i+1)%n_supergrid, j, 1, 1:-2]
+                # Fetch ghost cells from neighbours
+                n = n_supergrid
+                # lower line fetches from lower upper line
+                supergrid[i, j, 1:-1, 0] = supergrid[i, (j-1)%n, 1:-1, -2]
+                # upper line fetches from upper lower line
+                supergrid[i, j, 1:-1, -1] = supergrid[i, (j+1)%n, 1:-1, 1]
+                # left line fetches from left right line
+                supergrid[i, j, 0, 1:-1] = supergrid[(i-1)%n, j, -2, 1:-1]
+                # right line fetches from right left line
+                supergrid[i, j, -1, 1:-1] = supergrid[(i+1)%n, j, 1, 1:-1]
 
-            # corner points
-            #supergrid[i, j, 0, 0] = supergrid[i, (j+1)%n_supergrid, 1:-2, -2]
-            #supergrid[i, j, 0, -1] = supergrid[i, (j-1)%n_supergrid, 1:-2, 1]
-            #supergrid[i, j, -1, 0] = supergrid[(i+1)%n_supergrid, j, -2, 1:-2]
-            #supergrid[i, j, -1, -1] = supergrid[(i+1)%n_supergrid, j, 1, 1:-2]
+                # corner points
+                #supergrid[i, j, 0, 0] = supergrid[i, (j+1)%n_supergrid, 1:-2, -2]
+                #supergrid[i, j, 0, -1] = supergrid[i, (j-1)%n_supergrid, 1:-2, 1]
+                #supergrid[i, j, -1, 0] = supergrid[(i+1)%n_supergrid, j, -2, 1:-2]
+                #supergrid[i, j, -1, -1] = supergrid[(i+1)%n_supergrid, j, 1, 1:-2]
+
 
 
     flatten(flat, supergrid, n_supergrid, n_grid)
     imshow.set_data(flat)
 
-ani = FuncAnimation(fig, update, frames=np.linspace(0, 1000, 1), interval=300, repeat=True)
+ani = FuncAnimation(fig, update, frames=np.linspace(0, 2001, num=2000), interval=300, repeat=False)
 
 plt.show()
