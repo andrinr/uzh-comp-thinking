6629e3d3318f6abd052666d8bb11d4a6cb3ecc16
andrinr <andrinrehmann@gmail.com>
2022-11-29 11:00:52 +0100

    add nice plots

--- matrices/main.py
+++ matrices/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # check if two matrices can be multiplied
 def check_multiplication(sizeA, sizeB):
@@ -29,44 +30,55 @@ def gen_matrices(n, max_size = 100):
         matrices[i] = np.array(size)
     return matrices
 
-# generate matrices
-num_matrices = 200
-matrices = gen_matrices(num_matrices)
-print("random matrix sequence: ", matrices)
+def solve_dp(matrices):
+    num_matrices = matrices.shape[0]
+    # compute costs without any optimzation
+    naive_costs = 0
+    trailing_shape = matrices[0]
+    for i in range(num_matrices-2):
+        naive_costs += complexity(trailing_shape, matrices[i+1])
+        trailing_shape = get_size(trailing_shape, matrices[i+1])
 
-# compute costs without any optimzation
-naive_costs = 0
-trailing_shape = matrices[0]
-for i in range(num_matrices-2):
-    naive_costs += complexity(trailing_shape, matrices[i+1])
-    trailing_shape = get_size(trailing_shape, matrices[i+1])
+    # init DP tables
+    costs = np.full((num_matrices, num_matrices), np.inf)
+    sizes = np.zeros((num_matrices, num_matrices, 2))
+    sizes[1,:,:] = matrices
+    costs[:2,:] = 0
 
-# init DP tables
-costs = np.full((num_matrices, num_matrices), np.inf)
-sizes = np.zeros((num_matrices, num_matrices, 2))
-sizes[1,:,:] = matrices
-costs[:2,:] = 0
+    # evaluate DP tables, cost: O(n**3)
+    for l in range(2, num_matrices):
+        for i in range(num_matrices - l):
+            for a in range(1,l):
+                b = l - a
+                j = i + a
 
-# evaluate DP tables, cost: O(n**3)
-for l in range(2, num_matrices):
-    for i in range(num_matrices - l):
-        for a in range(1,l):
-            b = l - a
-            j = i + a
+                ij = complexity(sizes[a, i], sizes[b, j])
 
-            ij = complexity(sizes[a, i], sizes[b, j])
+                if costs[a,i] + costs[b,j] + ij < costs[l,i]:
+                    costs[l,i] = costs[a,i] + costs[b,j] + ij
+                    sizes[l,i] = get_size(sizes[a,i], sizes[b,j])
 
-            if costs[a,i] + costs[b,j] + ij < costs[l,i]:
-                costs[l,i] = costs[a,i] + costs[b,j] + ij
-                sizes[l,i] = get_size(sizes[a,i], sizes[b,j])
+    # fetch result from table
+    return costs, costs[num_matrices -1, 0], naive_costs
 
-# fetch result from table
-min_cost = costs[num_matrices-1,0]
-print('optimized multiplication cost is ' + format(min_cost / naive_costs  * 100, '.1f') +  ' percent of naive ordered multiplication costs.')
 
-import matplotlib.pyplot as plt
-plt.imshow(costs)
-plt.xlabel("sequence start matrix")
-plt.ylabel("sequence length")
+fig, axs = plt.subplots(nrows=4, ncols=4, figsize=(9, 12))
+fig.suptitle("DP tables with each" + str(100) + " random matrices.", fontsize=18)
+fig.tight_layout(pad=3.0)
+
+for k in range(4):
+    for l in range(4):
+
+        num_matrices = 100
+        matrices = gen_matrices(num_matrices)
+        costs, opt, naive = solve_dp(matrices)
+        speedup = naive / opt
+
+        axs[k,l].imshow(costs)
+
+        axs[k,l].set_xlabel("sequence start matrix")
+        axs[k,l].set_ylabel("sequence length")
+        axs[k,l].set_title("Speedup " + str(int(speedup)) + "x")
+
 plt.show()
 
